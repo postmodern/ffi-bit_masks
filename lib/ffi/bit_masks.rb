a784ba58e34e468e3c593c9fbc1ed4e93036d0de
Postmodern <postmodern.mod3@gmail.com>
2013-08-27 11:43:48 -0700

    Put the bit_mask method into the main file.

--- lib/ffi/bit_masks.rb
+++ lib/ffi/bit_masks.rb
@@ -1,2 +1,26 @@
 require 'ffi/bit_masks/bit_mask'
-require 'ffi/bit_masks/bit_masks'
+require 'ffi/bit_masks/version'
+
+module FFI
+  module BitMasks
+    #
+    # Defines a new bitmask.
+    #
+    # @param [Symbol] name
+    #
+    # @param [Hash{Symbol => Integer}] fields
+    #
+    # @param [Symbol] type
+    #   The underlying type.
+    #
+    # @return [BitMask]
+    #   The new bitmask.
+    #
+    def bit_mask(name,fields,type=:uint)
+      bit_mask = BitMask.new(type,fields)
+
+      typedef(name,bit_mask)
+      return bit_mask
+    end
+  end
+end
